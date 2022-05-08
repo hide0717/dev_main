47d9faf4321ddaead49a1b11aa9ac041b75b67db
JAY KWON <hide0717@nate.com>
2022-05-09 08:42:20 +0900

    pis로커밋

--- firstwork.java
+++ firstwork.java
@@ -3,6 +3,6 @@
 
 public class Hello {                                                                    
     public static void main(String[] args) {
-        System.out.println("Hello World22");
+        System.out.println("Hello World221");
     }
 }
